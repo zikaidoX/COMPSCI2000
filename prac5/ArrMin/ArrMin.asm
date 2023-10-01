5ba997570588b3e314e32328789e8ecc3ca29b9f
zikaidoX <kendokaieducation@gmail.com>
2023-10-01 22:01:35 +1030

    message

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -1,57 +1,69 @@
-// Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+    @R1
+    D=M-1
+    @R2
+    M=M+D
 
-// Initialize variables
-@0     // Set R0 to 0 (initial minimum value)
-D=A
-@R3
-M=D    // Store initial minimum value in R3
-@R1    // Load array start address into R1
-D=M
-@R4
-M=D    // Store array start address in R4
-@R2    // Load array length into R2
-D=M
-@R5
-M=D    // Store array length in R5
-@R4    // Load current element address into R4
-D=M
-@R6
-M=D    // Store current element address in R6
-
-// Loop through array to find minimum value
-(LOOP)
-    @R5 // Load array length into A register
-    D=M
-    @END_LOOP
-    D;JEQ // If array length is 0, end loop
-    @R4 // Load current element address into A register
-    D=M
-    @R3 // Load minimum value into D register
-    D=D-M
-    @CHECK_MIN
-    D;JGT // If current element is greater than minimum, check next element
-    @R4 // Load current element address into R6
-    M=D
-    @R3 // Store new minimum value in R3
-    M=D
-(CHECK_MIN)
-    @R6 // Load current element address into A register
-    D=M
-    @1  // Add 1 to current element address to get next element address
-    D=D+A
-    @R4 // Store next element address in R4
-    M=D
-    @R5 // Decrement array length
-    M=M-1
-    @LOOP // Jump to beginning of loop
-    0;JMP
-
-// End of loop
-(END_LOOP)
-    @R3 // Load minimum value into R0 (output)
-    D=M
+    @32767
+    D=A
     @R0
     M=D
-    @END
+
+(LOOP)
+(CHECK_TERMINATE)
+	@R1
+	D=M
+	@R2
+	D=D-M
+	@END
+	D;JGT
+	@R1
+	A=M
+	D=M
+    @ELEM_POS
+    D;JGE
+	@ELEM_NEG
+	0;JMP
+(UPDATE)
+	@R1
+	A=M
+	D=M
+	@R0
+	M=D
+(SKIP)
+	@R1
+	M=M+1
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
+
+(R0_NEG)
+    
+(R0_POS)
+    // subs
+    @R1
+	A=M
+	D=M
+	@R0
+	D=D-M // substraction, may cause Overflow!
+    @SKIP
+	D;JGE
+    @UPDATE
+    0;JMP
+
+(ELEM_NEG)
+    @R0
+    D=M
+    @R0_NEG
+    D;JLT
+    @UPDATE
+    0;JMP
+
+(ELEM_POS)
+    @R0
+    D=M
+    @R0_POS
+    D;JGE
+    @SKIP
     0;JMP
--- prac5/ArrSort/ArrSort.asm
+++ prac5/ArrSort/ArrSort.asm
@@ -1,48 +1,102 @@
-// Sorts the array of length R2 whose first element is at RAM[R1] in ascending order in place. Sets R0 to True (-1) when complete.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+    @R1 // the address of arr[0]
+    D=M-1 // substract 1 because the array starts from arr[0]
+    @R2 // the length
+    M=M+D // now R2 is the address of the last element
 
-@R1      // Load the address of the first element into A
-D=M      // Load the value at the first element into D
-@ENDLOOP // Label to mark the end of the loop
-0;JMP    // Jump to the main loop
-
-LOOP:
-    // Compare adjacent elements and swap if necessary
+(OUTER_LOOP)
+(CHECK_TERMINATE)
     @R1
-    D=M      // Load the value of the current element into D
-    @R1+1
-    D=D-M    // Compare with the next element
+    D=M
+    @R2
+    D=D-M
+    @FINISH
+    D;JGT
+    @R1
+    D=M
+    @R3 // use R3 as the index of the inner loop.
+    M=D+1 
 
-    // If D is positive or zero, the elements are in the correct order, so skip the swap
+
+(INNER_LOOP)
+(CHECK_INNER_END)
+    @R3
+    D=M
+    @R2
+    D=D-M
+    @INNER_FINISH
+    D;JGT
+    @R3 // use inner index to locate the element.
+    A=M
+    D=M // now D contains the element pointed by the inner index.
+    @ELEM_POS
     D;JGE
-
-    // Swap the elements
-    @R1      // Load the address of the first element into A
-    D=M      // Load the value at the first element into D
-    @R1+1    // Load the address of the second element into A
-    M=M-D    // Subtract the value of the first element from the second element
-    @R1      // Load the address of the first element into A
-    M=M+D    // Add the value of the second element to the first element
-
-ENDLOOP:
-    @R1      // Load the address of the first element into A
-    D=A      // Load the current address into D
-    @R2      // Load the length of the array into A
-    D=D-A    // Calculate (RAM address of first element + array length) - current address
-
-    // If D is zero or positive, we have reached the end of the array
-    D;JGE
-
-    // Otherwise, continue the loop
-    @LOOP
+    @ELEM_NEG
+    0;JMP
+(SWAP) // swap the value pointed by the inner and outer index pointer
+    @R1
+    A=M
+    D=M
+    @temp
+    M=D
+    @R3
+    A=M
+    D=M
+    @R1
+    A=M
+    M=D
+    @temp
+    D=M
+    @R3
+    A=M
+    M=D
+(SKIP)
+    @R3
+    M=M+1
+    @INNER_LOOP
     0;JMP
 
-// Done sorting, set R0 to True (-1)
-@R0
-M=-1
+(INNER_FINISH)
+    @R1
+    M=M+1
+    @OUTER_LOOP
+    0;JMP
 
-// Halt the program
-@END
-0;JMP
-
+(FINISH)
+    @R0
+    M=-1
 (END)
+    @END
+    0;JMP
+
+(REF_NEG)
+    
+(REF_POS)
+    // subs
+    @R3
+    A=M
+    D=M
+    @R1
+    A=M
+    D=D-M // substraction, may cause Overflow!
+    @SKIP
+    D;JGE
+    @SWAP
+    0;JMP
+
+(ELEM_NEG)
+    @R1
+    A=M
+    D=M
+    @REF_NEG
+    D;JLT
+    @SWAP
+    0;JMP
+
+(ELEM_POS)
+    @R1
+    A=M
+    D=M
+    @REF_POS
+    D;JGE
+    @SKIP
+    0;JMP
