85094dc2f85829a3351cc09937e7c8dccef7bd32
zikaidoX <kendokaieducation@gmail.com>
2023-10-01 21:30:14 +1030

    message

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -1,41 +1,78 @@
-// Multiplies R1 and R2 and stores the result in R0.
-// (R0, R1, R2, R3 refer to RAM[0], RAM[1], RAM[2], RAM[3] respectively.)
+    @sign
+    M=0
+(CHECK_R1)
+    @R1
+    D=M
+    @ABS_R1
+    D;JLT
+(CHECK_R2)
+    @R2
+    D=M
+    @ABS_R2
+    D;JLT
 
-// Initialize R0 to 0
-@0
-D=A
-@R0
-M=D
-
-// Initialize R3 to 0 (used as a counter)
-@0
-D=A
-@R3
-M=D
+(ENTRY)
+    @R1
+    D=M
+    @R2
+    D=D-M
+    @SWAP_R1R2
+    D;JLT
 
 (LOOP)
-// Check if R3 = R2
-@R3
-D=M
-@R2
-D=D-M
-@END
-D;JEQ
+    @R2
+    MD=M-1
+    @CHECK_SIGN
+    D;JLT
+    @R1
+    D=M
+    @R0
+    M=M+D
+    @LOOP
+    0;JMP
 
-// R0 = R0 + R1
-@R0
-D=M
-@R1
-D=D+M
-@R0
-M=D
-
-// R3 = R3 + 1
-@R3
-M=M+1
-
-// If R3 != R2, go back to LOOP
-@LOOP
-0;JMP
+(CHECK_SIGN)
+    @sign
+    D=M
+    @END
+    D;JEQ
+    @R0
+    M=-M
+    @END
+    0;JMP
 
 (END)
+    @END
+    0;JMP
+
+(ABS_R2)
+    @sign
+    M=!M
+    @R2
+    M=-M
+    @ENTRY
+    0;JMP
+
+(ABS_R1)
+    @sign
+    M=!M
+    @R1
+    M=-M
+    @CHECK_R2
+    0;JMP
+
+(SWAP_R1R2)
+    @R1
+    D=M
+    @temp
+    M=D
+    @R2
+    D=M
+    @R1
+    M=D
+    @temp
+    D=M
+    @R2
+    M=D
+    @LOOP
+    0;JMP
