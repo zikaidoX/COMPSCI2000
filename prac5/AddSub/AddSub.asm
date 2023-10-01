f9957066fe0b4553bfaf985ac801446f60177389
zikaidoX <kendokaieducation@gmail.com>
2023-10-01 23:02:08 +1030

    message

--- prac5/AddSub/AddSub.asm
+++ prac5/AddSub/AddSub.asm
@@ -1,17 +1,12 @@
 // Calculates R1 + R2 - R3 and stores the result in R0.
 // (R0, R1, R2, R3 refer to RAM[0], RAM[1], RAM[2], and RAM[3], respectively.)
 
-@R1
-D=M              // D = first number
-@R2
-D=D+M            // D = first number + second number
-@R3
-D=D-M            // D = D - third number
-
-// Store the result in R0 (RAM[0])
-@R0
-M=D
-
-// Output the Result
-@OUTPUT_D
-D=M
+  @R1
+  D=M              // D = first number
+  @R2
+  D=D+M            // D = first number + second number
+  @R3
+  D=D-M            // D = D - third number
+  @R0
+  M=D
+  @OUTPUT_D         // Output the Result
