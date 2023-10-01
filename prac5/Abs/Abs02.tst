3f968349d0f0b7bbf20004f8934c45f6da509406
zikaidoX <kendokaieducation@gmail.com>
2023-10-01 21:53:29 +1030

    message

--- prac5/Abs/Abs02.tst
+++ prac5/Abs/Abs02.tst
@@ -3,16 +3,16 @@
 // Appendix B of the book "The Elements of Computing Systems"
 
 load Abs.asm,
-output-file Abs00.out,
-compare-to Abs00.cmp,
+output-file Abs02.out,
+compare-to Abs02.cmp,
 output-list RAM[0]%D2.6.2 RAM[1]%D2.6.2;
 
 set PC 0,
 set RAM[0] 0,  // Set R0
-set RAM[1] -2;  // Set R1
+set RAM[1] 1;  // Set R1
 repeat 100 {
   ticktock;    // Run for 100 clock cycles
 }
-set RAM[1] -2,  // Restore arguments in case program used them
+set RAM[1] 1,  // Restore arguments in case program used them
 output;        // Output to file
 
--- prac5/AddSub/AddSub.asm
+++ prac5/AddSub/AddSub.asm
@@ -1,12 +1,32 @@
 // Calculates R1 + R2 - R3 and stores the result in R0.
 // (R0, R1, R2, R3 refer to RAM[0], RAM[1], RAM[2], and RAM[3], respectively.)
 
-  @R1
-  D=M              // D = first number
-  @R2
-  D=D-M            // D = first number - second number
-  @R3
-  D=M+D            // D = D +third number
-  @R0
-  M=D
-  @OUTPUT_D         //  Output the Result
+@R1
+D=M             // D = first number (R1)
+@R2
+D=D-M           // D = first number - second number (R1 - R2)
+@R3
+D=D+M           // D = D + third number (R1 - R2 + R3)
+@R0
+M=D             // R0 = R1 - R2 + R3
+
+// Check if R0 is negative and adjust if necessary
+@R0
+D=M             // D = R0
+@NEG_CHECK
+D;JLT           // If R0 is negative, jump to NEG_CHECK
+
+@OUTPUT_POSITIVE // Output the Result (for positive integers)
+0;JMP
+
+(NEG_CHECK)
+@OUTPUT_NEGATIVE // Output the Result (for negative integers)
+0;JMP
+
+(OUTPUT_POSITIVE)
+@OUTPUT_D        // Output the Result (for positive integers)
+0;JMP
+
+(OUTPUT_NEGATIVE)
+@OUTPUT_D_NEG    // Output the Result (for negative integers)
+0;JMP
